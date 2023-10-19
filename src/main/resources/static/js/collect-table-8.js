28d5c41ae6f4deb8cbd07592208cec985481a72d
Jacog00135 <2428207444@qq.com>
2023-10-19 18:29:20 +0800

    完成collect-table-7.html

---
+++ src/main/resources/static/js/collect-table-8.js
@@ -0,0 +1,103 @@
+(() => {
+    'use strict';
+
+    const adlForm = document.querySelector('form[name="adl"]');
+    const submitButton = adlForm.querySelector('[type="submit"');
+    const adlChildren = [
+        DropdownSelect(
+            '使用公共车辆',
+            'vehicles',
+            ['根本无法做', '需要帮助', '有些困难', '自己完全可以做'],
+            ['0', '1', '2', '3']
+        ),
+        DropdownSelect(
+            '行走',
+            'walk',
+            ['根本无法做', '需要帮助', '有些困难', '自己完全可以做'],
+            ['0', '1', '2', '3']
+        ),
+        DropdownSelect(
+            '做饭菜',
+            'cook',
+            ['根本无法做', '需要帮助', '有些困难', '自己完全可以做'],
+            ['0', '1', '2', '3']
+        ),
+        DropdownSelect(
+            '做家务',
+            'housework',
+            ['根本无法做', '需要帮助', '有些困难', '自己完全可以做'],
+            ['0', '1', '2', '3']
+        ),
+        DropdownSelect(
+            '吃药',
+            'medicine',
+            ['根本无法做', '需要帮助', '有些困难', '自己完全可以做'],
+            ['0', '1', '2', '3']
+        ),
+        DropdownSelect(
+            '吃饭',
+            'eat',
+            ['根本无法做', '需要帮助', '有些困难', '自己完全可以做'],
+            ['0', '1', '2', '3']
+        ),
+        DropdownSelect(
+            '穿衣',
+            'dress',
+            ['根本无法做', '需要帮助', '有些困难', '自己完全可以做'],
+            ['0', '1', '2', '3']
+        ),
+        DropdownSelect(
+            '梳头、刷牙等',
+            'hair',
+            ['根本无法做', '需要帮助', '有些困难', '自己完全可以做'],
+            ['0', '1', '2', '3']
+        ),
+        DropdownSelect(
+            '洗衣',
+            'laundry',
+            ['根本无法做', '需要帮助', '有些困难', '自己完全可以做'],
+            ['0', '1', '2', '3']
+        ),
+        DropdownSelect(
+            '洗澡',
+            'shower',
+            ['根本无法做', '需要帮助', '有些困难', '自己完全可以做'],
+            ['0', '1', '2', '3']
+        ),
+        DropdownSelect(
+            '购物',
+            'shopping',
+            ['根本无法做', '需要帮助', '有些困难', '自己完全可以做'],
+            ['0', '1', '2', '3']
+        ),
+        DropdownSelect(
+            '定时上厕所',
+            'bathroom',
+            ['根本无法做', '需要帮助', '有些困难', '自己完全可以做'],
+            ['0', '1', '2', '3']
+        ),
+        DropdownSelect(
+            '打电话',
+            'phone',
+            ['根本无法做', '需要帮助', '有些困难', '自己完全可以做'],
+            ['0', '1', '2', '3']
+        ),
+        DropdownSelect(
+            '处理自己钱物',
+            'money',
+            ['根本无法做', '需要帮助', '有些困难', '自己完全可以做'],
+            ['0', '1', '2', '3']
+        )
+    ];
+    adlChildren.forEach((obj) => {
+        adlForm.insertBefore(obj.getNode(), submitButton);
+    });
+    adlForm.addEventListener('submit', submitForm);
+
+    function submitForm(e) {
+        e.preventDefault();
+
+        // 获取表单数据
+        const data = {};
+    }
+})();
