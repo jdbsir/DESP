c0bce6beb4da7a49e467000c013d592f3bdcf413
Jacog00135 <2428207444@qq.com>
2023-11-02 09:53:22 +0800

    搜索功能

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,108 +1,97 @@
 (() => {
     'use strict';
 
-    // 生成测试数据
-    function getRandomSubjectId() {
-        const subjectName = [String(Math.floor(Math.random() * 9) + 1)];
-        for (let i = 0; i < 17; i++) {
-            subjectName.push(String(Math.floor(Math.random() * 10)));
-        }
-        return subjectName.join('');
-    }
-    function getRandomSubjectName() {
-        const str = [];
-        for (let i = 0; i < 2; i++) {
-            let r = Math.floor(Math.random() * 52);
-            if (r < 26) {
-                str.push(String.fromCharCode('A'.charCodeAt() + r));
-            } else {
-                str.push(String.fromCharCode('a'.charCodeAt() + r - 26))
-            }
-        }
-        return '名字' + str.join('');
-    }
-    function getRandomTime() {
-        const max = +new Date();
-        const min = max - 3600 * 24 * 365 * 1000;
-        const timestamp = Math.floor(Math.random() * (max - min)) + min;
-        const date = new Date(timestamp);
-        const year = date.getFullYear();
-        const month = date.getMonth() + 1;
-        const day = date.getDate();
-        const hours = date.getHours();
-        const minutes = date.getMinutes();
-        const seconds = date.getSeconds();
-        return [timestamp, `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`];
-    }
-    const data = {};
-    const subjectNumber = 4;
-    const recordNumber = 6;
-    const subjectIdArray = [];
-    let globalId = 0;
-    for (let i = 0; i < subjectNumber; i++) {
-        let subjectId = getRandomSubjectId();
-        let [timestamp, time] = getRandomTime();
-        subjectIdArray.push(subjectId);
-        data[subjectId] = [{
-            'id': globalId,
-            'subject_id': subjectId,
-            'name': getRandomSubjectName(),
-            'MOCA': Math.floor(Math.random() * 31),
-            'MMSE': Math.floor(Math.random() * 31),
-            'GDTOTAL': Math.floor(Math.random() * 31),
-            'NPISCORE': Math.floor(Math.random() * 37),
-            'ADLSCORE': Math.floor(Math.random() * 57),
-            'timestamp': timestamp,
-            'time': time
-        }];
-        globalId = globalId + 1;
-    }
-    for (let i = 0; i < recordNumber - subjectNumber; i++) {
-        let randomSubjectId = subjectIdArray[Math.floor(Math.random() * subjectNumber)];
-        let [timestamp, time] = getRandomTime();
-        let recordArray = data[randomSubjectId];
-        data[randomSubjectId].push({
-            'id': globalId,
-            'subject_id': randomSubjectId,
-            'name': data[randomSubjectId][0]['name'],
-            'MOCA': Math.floor(Math.random() * 31),
-            'MMSE': Math.floor(Math.random() * 31),
-            'GDTOTAL': Math.floor(Math.random() * 31),
-            'NPISCORE': Math.floor(Math.random() * 37),
-            'ADLSCORE': Math.floor(Math.random() * 57),
-            'timestamp': timestamp,
-            'time': time
-        });
-        globalId = globalId + 1;
-    }
-    for (let subjectId in data) {
-        data[subjectId].sort((a, b) => {
-            return b.timestamp - a.timestamp;
-        });
-    }
-    console.log(data);
+    requestDataAndRenderPage();
 
-    // 填充到html
-    const baseInfo = document.getElementById('base-info');
-    baseInfo.querySelector('.subject-number .text').innerHTML = subjectNumber;
-    baseInfo.querySelector('.record-number .text').innerHTML = recordNumber;
-    const recordList = document.getElementById('record-list');
-    for (let subjectId in data) {
-        let items = [];
-        data[subjectId].forEach((record) => {
-            items.push(`
-                <li class="item">
-                    <a href="/collect-table-1.html?id=${record['id']}&readonly=1">记录时间：${record['time']}</a>
-                </li>
-            `);
+    // 按钮事件
+    document.getElementById('unfold-all-record').addEventListener('click', unfoldAllRecord);
+    document.getElementById('fold-all-record').addEventListener('click', foldAllRecord);
+    document.getElementById('back-homepage').addEventListener('click', backHomepage);
+
+    function requestDataAndRenderPage() {
+        const searchName = parseQueryParam()['search_name'];
+        let url = '/query_history_record';
+        let searchResult = false;
+        if (searchName !== undefined) {
+            url = `${url}?search_name=${searchName}`;
+            searchResult = true;
+        }
+        ajaxGetJson(url).then((response) => {
+            if (response.code !== 1) {
+                alert(response.msg);
+                return undefined;
+            }
+
+            console.log(response.data);
+            renderHTML(response.data, searchResult);
         });
-        recordList.appendChild(htmlToNode(`
-            <li class="subject-item" data-subject-id="${subjectId}">
-                <details>
-                    <summary>${data[subjectId][0]['name']}（${data[subjectId][0]['subject_id']}）</summary>
-                    <ul class="items">${items.join('')}</ul>
-                </details>
-            </li>
-        `));
+    }
+
+    function renderHTML(data, searchResult) {
+        // 记录数量
+        const baseInfo = document.getElementById('base-info');
+        let showQSArray = ['.subject-number .text', '.record-number .text'];
+        let hiddenQSArray = ['.search-subject-number .text', '.search-record-number .text'];
+        if (searchResult) {
+            const tmp = showQSArray;
+            showQSArray = hiddenQSArray;
+            hiddenQSArray = tmp;
+        }
+        const subjectNumberText = baseInfo.querySelector(showQSArray[0]);
+        const recordNumberText = baseInfo.querySelector(showQSArray[1]);
+        subjectNumberText.innerHTML = data['subject_number'];
+        recordNumberText.innerHTML = data['record_number'];
+        subjectNumberText.parentElement.classList.remove('hidden');
+        recordNumberText.parentElement.classList.remove('hidden');
+        baseInfo.querySelector(hiddenQSArray[0]).parentElement.classList.add('hidden');
+        baseInfo.querySelector(hiddenQSArray[1]).parentElement.classList.add('hidden');
+
+        // 是否显示搜索框、返回主页按钮
+        if (searchResult) {
+            document.querySelector('form[name="search-record"]').classList.add('hidden');
+            document.getElementById('back-homepage').classList.remove('hidden');
+        }
+
+        // 记录列表
+        const recordList = document.getElementById('record-list');
+        data = data['history_record'];
+        for (let subjectId in data) {
+            let items = [];
+            data[subjectId].forEach((record) => {
+                items.push(`
+                    <li class="item">
+                        <a href="/collect-table-1.html?id=${record['id']}&readonly=1">记录时间：${record['time']}</a>
+                    </li>
+                `);
+            });
+            recordList.appendChild(htmlToNode(`
+                <li class="subject-item" data-subject-id="${subjectId}">
+                    <details>
+                        <summary>${data[subjectId][0]['name']}（${data[subjectId][0]['subject_id']}）</summary>
+                        <ul class="items">${items.join('')}</ul>
+                    </details>
+                </li>
+            `));
+        }
+    }
+
+    function unfoldAllRecord(e) {
+        document.querySelectorAll('#record-list > .subject-item details').forEach((details) => {
+            if (details.getAttribute('open') === null) {
+                details.querySelector('summary').click();
+            }
+        });
+    }
+
+    function foldAllRecord(e) {
+        document.querySelectorAll('#record-list > .subject-item details').forEach((details) => {
+            if (details.getAttribute('open') !== null) {
+                details.querySelector('summary').click();
+            }
+        });
+    }
+
+    function backHomepage(e) {
+        location.href = '/';
     }
 })();
