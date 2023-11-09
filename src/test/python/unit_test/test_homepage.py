5144dbf5194a2a57543065fde70ea58879fe344b
Jacog00135 <2428207444@qq.com>
2023-11-09 17:29:51 +0800

    test_homepage

--- src/test/python/unit_test/test_homepage.py
+++ src/test/python/unit_test/test_homepage.py
@@ -22,7 +22,7 @@ class TestHomepage(TestCase):
         """查询历史记录"""
         self.login()
 
-        data = self.generate_test_data(2)['1']
+        data = self.generate_test_data(3)['2']
 
         url = Config.get_route('query_history_record')
         response = self.client_session.get(url)
@@ -37,4 +37,30 @@ class TestHomepage(TestCase):
         self.assertTrue(client_history_record is not None)
         self.assertTrue(client_data.get('subject_number') == len(client_history_record))
         self.assertTrue(client_data.get('record_number') == sum([len(v) for v in client_history_record.values()]))
-        for 
+        for client_id_card, id_card in zip(client_history_record.keys(), data.keys()):
+            self.assertTrue(client_id_card, id_card)
+
+    def test_anonymous_search(self):
+        """未登录状态下无法搜索"""
+        self.assertFalse(self.is_login())
+
+        data = self.generate_test_data(3)['2']
+
+        url = Config.get_route('search') + '?search_subject_id=45'
+        response = self.client_session.get(url)
+        self.assertTrue(response.status_code == 200)
+        self.assertFalse(response.json.get('code', 0) == 1)
+
+    def test_search(self):
+        """搜索"""
+        self.login()
+
+        data = self.generate_test_data(3)['2']
+        search_subject_id = '45'
+        filter_id_card = list(filter(lambda k: search_subject_id in k, data.keys()))
+
+        url = Config.get_route('search') + '?search_subject_id={}'.format(search_subject_id)
+        response = self.client_session.get(url)
+        self.assertTrue(response.status_code == 200)
+        self.assertTrue(response.json.get('code', 0) == 1)
+        # TODO
